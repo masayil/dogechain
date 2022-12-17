742fef3de341d655b28ad8b257c25250940315a4
DarianShawn <DarianShawn007@gmail.com>
2022-12-17 16:48:02 +0800

    Refactor leveldb test cases

--- helper/kvdb/leveldb_test.go
+++ helper/kvdb/leveldb_test.go
@@ -33,129 +33,121 @@ func createTestDB(t *testing.T) KVBatchStorage {
 	return db
 }
 
-func TestLevelDB(t *testing.T) {
+func Test_LevelDB_GetSet(t *testing.T) {
 	t.Parallel()
 
-	t.Run("test KVStorage Get/Set", func(t *testing.T) {
-		t.Parallel()
+	db := createTestDB(t)
+	defer db.Close()
 
-		db := createTestDB(t)
-		defer db.Close()
+	var (
+		key   = []byte("hello")
+		value = []byte("world")
+	)
 
-		if err := db.Set([]byte("hello"), []byte("world")); err != nil {
-			t.Fatal(err)
-		}
+	err := db.Set(key, value)
+	assert.NoError(t, err)
 
-		v, exist, err := db.Get([]byte("hello"))
-		if err != nil {
-			t.Fatal(err)
-		}
+	v, exist, err := db.Get(key)
+	assert.NoError(t, err)
+	assert.True(t, exist)
+	assert.Equal(t, value, v)
+}
 
+func Test_LevelDB_BatchWrite(t *testing.T) {
+	t.Parallel()
+
+	db := createTestDB(t)
+	defer db.Close()
+
+	var (
+		length = 100
+		keys   = make([][]byte, 0, length)
+		values = make([][]byte, 0, length)
+		batch  = db.NewBatch()
+	)
+
+	for i := 0; i < length; i++ {
+		// random seed
+		seed := rand.NewSource(time.Now().Unix())
+		r := rand.New(seed)
+		// k, v
+		key := make([]byte, 32)
+		value := make([]byte, 128)
+		// random read
+		r.Read(key)
+		r.Read(value)
+		// save value
+		keys = append(keys, key)
+		values = append(values, value)
+		// batch it
+		batch.Set(key, value)
+	}
+
+	// batch write
+	err := batch.Write()
+	assert.NoError(t, err)
+
+	for i := 0; i < length; i++ {
+		val, exist, err := db.Get(keys[i])
+		assert.NoError(t, err)
 		assert.True(t, exist)
+		assert.Equal(t, values[i], val)
+	}
+}
 
-		if string(v) != "world" {
-			t.Fatal("value not equal")
-		}
-	})
+func Test_LevelDB_Iterator(t *testing.T) {
+	t.Parallel()
 
-	t.Run("test KVStorage Batch Write", func(t *testing.T) {
-		t.Parallel()
+	db := createTestDB(t)
+	defer db.Close()
 
+	var (
+		length = 100
+		keys   = make([][]byte, 0, length)
+		values = make([][]byte, 0, length)
+		batch  = db.NewBatch()
+	)
+
+	for i := 0; i < length; i++ {
+		// random seed
 		seed := rand.NewSource(time.Now().Unix())
+		r := rand.New(seed)
+		// key
+		key := make([]byte, 32)
+		r.Read(key)
+		// key prefix
+		prefix := make([]byte, 4)
+		binary.LittleEndian.PutUint32(prefix, uint32(i))
+		// append prefix
+		key = append(prefix[:], key...)
+		// value
+		value := make([]byte, 128)
+		r.Read(value)
+		// k, v
+		keys = append(keys, key)
+		values = append(values, value)
+		// batch it
+		batch.Set(key, value)
+	}
 
-		db := createTestDB(t)
-		defer db.Close()
+	if err := batch.Write(); err != nil {
+		t.Fatal(err)
+	}
 
-		keys := [][]byte{{}}
-		values := [][]byte{{}}
+	iter := db.NewIterator(nil, nil)
+	defer iter.Release()
 
-		{
-			batch := db.NewBatch()
-			r := rand.New(seed)
+	count := 0
+	for iter.Next() {
+		k := iter.Key()
+		v := iter.Value()
 
-			for i := 0; i < 100; i++ {
-				key := make([]byte, 32)
-				r.Read(key)
+		assert.Equal(t, keys[count], k)
+		assert.Equal(t, values[count], v)
 
-				keys = append(keys, key)
+		count++
+	}
 
-				value := make([]byte, 128)
-				r.Read(value)
-
-				values = append(values, value)
-
-				batch.Set(key, value)
-			}
-
-			if err := batch.Write(); err != nil {
-				t.Fatal(err)
-			}
-		}
-
-		{
-			for i := 1; i < len(keys); i++ {
-				val, exist, err := db.Get(keys[i])
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				assert.True(t, exist)
-				assert.Equal(t, values[i], val)
-			}
-		}
-	})
-
-	t.Run("test KVStorage Iteration", func(t *testing.T) {
-		t.Parallel()
-
-		seed := rand.NewSource(time.Now().Unix())
-
-		db := createTestDB(t)
-		defer db.Close()
-
-		keys := [][]byte{{}}
-		values := [][]byte{{}}
-
-		{
-			batch := db.NewBatch()
-			r := rand.New(seed)
-
-			for i := 0; i < 100; i++ {
-				key := make([]byte, 32)
-				r.Read(key)
-
-				prefix := make([]byte, 4)
-				binary.LittleEndian.PutUint32(prefix, uint32(i))
-
-				key = append(prefix[:], key...)
-
-				keys = append(keys, key)
-
-				value := make([]byte, 128)
-				r.Read(value)
-
-				values = append(values, value)
-
-				batch.Set(key, value)
-			}
-
-			if err := batch.Write(); err != nil {
-				t.Fatal(err)
-			}
-		}
-
-		{
-			iter := db.NewIterator(nil)
-			defer iter.Release()
-
-			iter.First()
-
-			for i := 1; i < len(keys); i++ {
-				assert.Equal(t, keys[i], iter.Key())
-				assert.Equal(t, values[i], iter.Value())
-
-				iter.Next()
-			}
-		}
-	})
+	assert.Equal(t, length, count)
+	assert.NoError(t, iter.Error())
 }
