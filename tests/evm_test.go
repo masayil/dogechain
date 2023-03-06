f67e7b29c5cee364b27ed8716a6088fb283c6c3c
DarianShawn <DarianShawn007@gmail.com>
2023-03-06 18:50:14 +0800

    Create eth evm tests with snapshot enable

--- tests/evm_test.go
+++ tests/evm_test.go
@@ -193,3 +193,142 @@ func TestEVM(t *testing.T) {
 func vmTestBlockHash(n uint64) types.Hash {
 	return crypto.Keccak256Hash([]byte(big.NewInt(int64(n)).String()))
 }
+
+func TestEVMWithSnapshot(t *testing.T) {
+	t.Parallel()
+
+	folders, err := listFolders(vmTests)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	long := []string{
+		"loop-",
+		"gasprice",
+		"origin",
+	}
+
+	for _, folder := range folders {
+		folder := folder
+		t.Run(folder, func(t *testing.T) {
+			t.Parallel()
+
+			files, err := listFiles(folder)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for _, file := range files {
+				if !strings.HasSuffix(file, ".json") {
+					continue
+				}
+
+				data, err := ioutil.ReadFile(file)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				var vmcases map[string]*VMCase
+				if err := json.Unmarshal(data, &vmcases); err != nil {
+					t.Fatal(err)
+				}
+
+				for name, cc := range vmcases {
+					if contains(long, name) && testing.Short() {
+						t.Skip()
+						continue
+					}
+					testVMCaseWithSnapshot(t, name, cc)
+				}
+			}
+		})
+	}
+}
+
+func testVMCaseWithSnapshot(t *testing.T, name string, c *VMCase) {
+	t.Helper()
+
+	env := c.Env.ToEnv(t)
+	env.GasPrice = types.BytesToHash(c.Exec.GasPrice.Bytes())
+	env.Origin = c.Exec.Origin
+
+	triedb, s, _, root, err := buildState(c.Pre)
+	assert.NoError(t, err)
+
+	snaps, err := buildSnapshotTree(triedb, root)
+	assert.NoError(t, err)
+
+	config := mainnetChainConfig.Forks.At(uint64(env.Number))
+
+	executor := state.NewExecutor(&mainnetChainConfig, hclog.NewNullLogger(), s)
+	executor.GetHash = func(*types.Header) func(i uint64) types.Hash {
+		return vmTestBlockHash
+	}
+
+	// set executor snapshot to test state features
+	executor.SetSnaps(snaps)
+
+	e, _ := executor.BeginTxn(root, c.Env.ToHeader(t), env.Coinbase)
+	ctx := e.ContextPtr()
+	ctx.GasPrice = types.BytesToHash(env.GasPrice.Bytes())
+	ctx.Origin = env.Origin
+
+	evmR := evm.NewEVM()
+
+	code := e.GetCode(c.Exec.Address)
+	contract := runtime.NewContractCall(
+		1,
+		c.Exec.Caller,
+		c.Exec.Caller,
+		c.Exec.Address,
+		c.Exec.Value,
+		c.Exec.GasLimit,
+		code,
+		c.Exec.Data,
+	)
+
+	result := evmR.Run(contract, e, &config)
+
+	if c.Gas == "" {
+		if result.Succeeded() {
+			t.Fatalf("gas unspecified (indicating an error), but VM returned no error")
+		}
+
+		if result.GasLeft > 0 {
+			t.Fatalf("gas unspecified (indicating an error), but VM returned gas remaining > 0")
+		}
+
+		return
+	}
+
+	// check return
+	if c.Out == "" {
+		c.Out = "0x"
+	}
+
+	if ret := hex.EncodeToHex(result.ReturnValue); ret != c.Out {
+		t.Fatalf("return mismatch: got %s, want %s", ret, c.Out)
+	}
+
+	txn := e.Txn()
+
+	// check logs
+	if logs := rlpHashLogs(txn.Logs()); logs != types.StringToHash(c.Logs) {
+		t.Fatalf("logs hash mismatch: got %x, want %x", logs, c.Logs)
+	}
+
+	// check state
+	for addr, alloc := range c.Post {
+		for key, val := range alloc.Storage {
+			if have, err := txn.GetState(addr, key); err != nil || have != val {
+				t.Fatalf("wrong storage value at %s:\n  got  %s\n  want %s\n at address %s\n err: %v",
+					key, have, val, addr, err)
+			}
+		}
+	}
+
+	// check remaining gas
+	if expected := stringToUint64T(t, c.Gas); result.GasLeft != expected {
+		t.Fatalf("gas left mismatch: got %d want %d", result.GasLeft, expected)
+	}
+}
